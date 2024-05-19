ce2d1eb357b01f397fc2f204af0c058f11f7f2eb
Parag <paragrajput52@gmail.com>
2024-05-19 11:08:34 +0530

    Initial commit

--- data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo su
-yum update -y
-yum install git -y 
-yum install httpd -y
-git clone https://github.com/Filgeary/demo-website-skyline.git
-cp -rn demo-website-skyline/* /var/www/html/
-systemctl restart httpd && systemctl enable httpd
--- main.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-  region     = var.region
-  access_key = "<AWS_ACCESS_KEY_ID>"
-  secret_key = "<AWS_SECRET_ACCESS_KEY>"
-}
