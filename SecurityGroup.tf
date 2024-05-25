ff58f952e15c6ab3740bd37b13db82e52b5407d8
paragrajput <94896763+paragrajput@users.noreply.github.com>
2024-05-25 17:56:30 +0530

    Update mirth port number

--- SecurityGroup.tf
+++ SecurityGroup.tf
@@ -21,8 +21,14 @@ resource "aws_security_group" "ec2-server-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8443
+    to_port     = 8443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
